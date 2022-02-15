b533a5293c048c000445788e539c30bcf9f1bfef
Casey Williams <caseyjw@mail.com>
2022-02-15 12:54:07 +0000

    Implement election start. Todo: receive votes
    Co-authored-by: Alexandru Moraru <moraru210@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -12,9 +12,10 @@ SETUP     = default	# one of default, slower, faster, etc
 # AppendEntries(areq, arep, atim), Vote(vreq, vrep, vall), Election(etim), DB(dreq, drep), Client(creq, crep)
 # Prefixes + for send/send_after,  - for receive
 DEBUG_OPTIONS = "+areq -areq +arep -arep +vreq +vall -vreq +vrep -vrep +atim -atim +etim -etim +dreq -dreq +drep -drep -creq -crep"
-DEBUG_OPTIONS = "none"
+# DEBUG_OPTIONS = "!inf !snd ?rec"
+DEBUG_OPTIONS = ""
 
-DEBUG_LEVEL   = 0
+DEBUG_LEVEL   = 1
 
 START     = Raft.start
 HOST	 := 127.0.0.1
--- lib/server.ex
+++ lib/server.ex
@@ -21,32 +21,60 @@ end # start
 
 # _________________________________________________________ next()
 def next(s) do
-
   s = receive do
 
-    { :APPEND_ENTRIES_REQUEST, msg } ->
-       # omitted  
+    # { :APPEND_ENTRIES_REQUEST, msg } ->
+    #    # omitted
 
-    { :APPEND_ENTRIES_REPLY, msg } ->
-      # omitted
+    # { :APPEND_ENTRIES_REPLY, msg } ->
+    #   # omitted
 
-    { :VOTE_REQUEST, msg } ->
-      # omitted
+    { :VOTE_REQUEST, { q, term } } ->
+      new_s = if term > s.curr_term do
+        State.stepdown(s, term)
+      else
+        s
+      end
+      if (term == new_s.curr_term and (new_s.voted_for == q or new_s.voted_for == nil)) do
+        voted_s = State.voted_for(new_s, q)
+        |> Timer.restart_election_timer()
+        send q, { :VOTE_REPLY, { self(), term, q}} # TODO: double check this
+        voted_s
+      else
+        new_s
+      end
 
-    { :VOTE_REPLY, msg } ->
-      # omitted
-    
-    { :ELECTION_TIMEOUT, msg } ->
-      # omitted
-    
-    { :APPEND_ENTRIES_TIMEOUT, msg } ->
-      # omitted
+    # { :VOTE_REPLY, msg } ->
+    #   # omitted
 
-    { :CLIENT_REQUEST, msg } ->                          
-       # omitted
- 
-    unexpected ->
-      # omitted
+    { :ELECTION_TIMEOUT, {curr_term, curr_election} } ->
+      unless s.role == :LEADER do
+        new_s = Timer.restart_election_timer(s)
+        |> State.inc_election()
+        |> State.inc_term()
+        |> State.role(:CANDIDATE)
+        |> State.vote_for_self()
+        |> Debug.info("Cancelling all append entries timers")
+        |> Timer.cancel_all_append_entries_timers()
+        for q <- s.servers do
+          send self(), { :APPEND_ENTRIES_TIMEOUT, { q } }
+          Debug.sent(s, { :APPEND_ENTRIES_TIMEOUT, { q } })
+        end
+        new_s
+      end
+
+    { :APPEND_ENTRIES_TIMEOUT, { q } } ->
+      if s.role == :CANDIDATE do
+        new_s = Timer.restart_append_entries_timer(s, q)
+        send q, { :VOTE_REQUEST, { self(), new_s.curr_term } }
+        Debug.sent(new_s, { :VOTE_REQUEST, { self(), new_s.curr_term } })
+      end
+
+    # { :CLIENT_REQUEST, msg } ->
+    #    # omitted
+
+    # unexpected ->
+    #   # omitted
 
   end # receive
 
@@ -55,4 +83,3 @@ def next(s) do
 end # next
 
 end # Server
-
